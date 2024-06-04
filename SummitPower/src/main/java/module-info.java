3b42b46d7e393111eaa80cc6e65e0f8b5e322fa2
Khalid <kibwalid@gmail.com>
2024-06-04 21:04:14 +0600

    Added base project files

---
+++ SummitPower/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.iub.summitpower {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.iub.summitpower to javafx.fxml;
+    exports com.iub.summitpower;
+}
