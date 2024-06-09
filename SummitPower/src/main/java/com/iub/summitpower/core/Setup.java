5911c7a443e5c6e148f1793b2a3131e0fcd61e48
Khalid <kibwalid@gmail.com>
2024-06-09 14:17:36 +0600

    Added Core components which can be reused or inherited by internal or functional features

---
+++ SummitPower/src/main/java/com/iub/summitpower/core/Setup.java
@@ -0,0 +1,9 @@
+package com.iub.summitpower.core;
+
+public class Setup {
+    public void injectDependencies() {
+    }
+
+    public void createDefaultUsers() {
+    }
+}
--- SummitPower/src/main/java/module-info.java
+++ SummitPower/src/main/java/module-info.java
@@ -3,14 +3,14 @@ module com.iub.summitpower {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+    requires jdk.jdi;
 
     opens com.iub.summitpower to javafx.fxml;
     exports com.iub.summitpower;
+    exports com.iub.summitpower.features.unlogged.splash;
+    opens com.iub.summitpower.features.unlogged.splash to javafx.fxml;
 }
