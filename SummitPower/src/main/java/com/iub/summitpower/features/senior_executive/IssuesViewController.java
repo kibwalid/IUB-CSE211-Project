34d0253b9c5cdc147445087449b172f1b009ff17
Khalid Ibnul <kibwalid@gmail.com>
2024-07-22 14:36:31 +0600

    Added issues list fxml design

---
+++ SummitPower/src/main/java/com/iub/summitpower/features/senior_executive/IssuesViewController.java
@@ -0,0 +1,57 @@
+package com.iub.summitpower.features.senior_executive;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class IssuesViewController {
+
+
+    @FXML
+    public Label activeIssuesLabel;
+
+    @FXML
+    public TableColumn<?, ?> costToFixCol;
+
+    @FXML
+    public TableColumn<?, ?> deadlineCol;
+
+    @FXML
+    public Button detailsButton;
+
+    @FXML
+    public Label fixedIssuesLabel;
+
+    @FXML
+    public BarChart<?, ?> issuesByMonthBarChart;
+
+    @FXML
+    public TableColumn<?, ?> nameCol;
+
+    @FXML
+    public TableView<?> projectTableView;
+
+    @FXML
+    public TableColumn<?, ?> qaManagerCol;
+
+    @FXML
+    public TableColumn<?, ?> statusCol;
+
+    @FXML
+    public void onDetailsButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void initialize() {
+
+
+    }
+
+}
